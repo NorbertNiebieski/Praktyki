b341fa51bccdd39ed257cb73a75c0093d97b47a4
Norbert Nawrocki <norek784@gmail.com>
2020-09-18 15:36:29 +0200

    adding IPbus test class

---
+++ IPbus_Test_Class_SV/IPbus_Test_Class_SV.sv
@@ -0,0 +1,32 @@
+interface IPbusInterface( 	input  ipb_in,
+											output  ipb_out);
+	
+	initial                            
+	begin
+
+		ipb_out.ipb_wdata = 0'b1;
+		ipb_out.ipb_addr = 0'b1;
+		ipb_out.ipb_write = 0'b1;
+		ipb_out.ipb_strobe = 0'b1;
+		ipb_in.data = 0'b1;
+		ipb_in.ipb_rdata = 0'b1;
+		ipb_in.ipb_ack = 0'b1;
+		ipb_in.ipb_err = 0'b1;
+
+	end
+                           
+endinterface: IPbusInterface
+
+
+class IPbusTestClass;
+
+	local virtual IPbusInterface  IPbusInterface1;
+ 
+    task assignInterface( virtual IPbusInterface  IPbusInterface1);
+        
+		this.IPbusInterface1 = IPbusInterface1;    
+    
+	endtask: assignInterface
+  
+
+endclass: IPbusTestClass
