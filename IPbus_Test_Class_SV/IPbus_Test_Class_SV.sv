4e5c51e3cae005a953052b2afd8f9994b2decf19
Norbert Nawrocki <norek784@gmail.com>
2020-09-23 17:15:42 +0200

    adding write and read

--- IPbus_Test_Class_SV/IPbus_Test_Class_SV.sv
+++ IPbus_Test_Class_SV/IPbus_Test_Class_SV.sv
@@ -27,5 +27,40 @@ class IPbusTestClass;
 		this.IPbusInterface1 = IPbusInterface1;    
     
 	endtask: assignInterface
+	
+	function void write (string filepath, string text)
+		
+		int fileDescriptor;
+		
+		fileDescriptor = $fopen(filepath, "w");
+		if (fd)  $display("File was opened successfully : %0d", fileDescriptor);
+		else     $display("File was NOT opened successfully : %0d", ffileDescriptor);
+		
+		$fdisplay (fileDescriptor, text);
+		
+		$fclose(fileDescriptor);
+		
+	endfunction
+	
+		function string read(string filepath)
+		
+		int fileDescriptor;
+		string line, text;
+		
+		fileDescriptor = $fopen(filepath, "r");
+		if (fd)  $display("File was opened successfully : %0d", fileDescriptor);
+		else     $display("File was NOT opened successfully : %0d", ffileDescriptor);
+		
+		while (!$feof(fd)) begin
+		$fgets(line, fd);
+		text = {text, line};
+		$display ("Line: %s", line);
+		end
+		
+		$fclose(fileDescriptor);
+		
+		return text
+			
+	endfunction
   
 endclass: IPbusTestClass
