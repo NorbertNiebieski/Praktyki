3a028bdfa390a0ab3f446ad50c82b5b5f8beb91b
Norbert Nawrocki <norek784@gmail.com>
2020-09-25 18:22:06 +0200

    adding ram module

--- IPbus_Test_Class_SV/IPbus_Test_Class_SV.sv
+++ IPbus_Test_Class_SV/IPbus_Test_Class_SV.sv
@@ -1,85 +1,25 @@
-interface IPbusInterface( 	input  ipb_in,
-				output  ipb_out);
-	
-	initial                            
-	begin
-
-		ipb_out.ipb_wdata = 1'b1;
-		ipb_out.ipb_addr = 1'b1;
-		ipb_out.ipb_write = 1'b1;
-		ipb_out.ipb_strobe = 1'b1;
-		ipb_in.data = 1'b1;
-		ipb_in.ipb_rdata = 1'b1;
-		ipb_in.ipb_ack = 1'b1;
-		ipb_in.ipb_err = 1'b1;
-
-	end
-                           
-endinterface: IPbusInterface
+module ipb_intf(
+  
+  virtual IPb_intf intf_in_module; 
+  virtual IPb_intf intf_out_module;
+  
+endmodule
 
 
-class IPbusTestClass;
-
-	local virtual IPbusInterface  IPbusInterface1;
+ task ipb_writing();    
+     
+            $display("%0d", ipb_addr );
+            
+            intf.intf_out_module.ipb_wdata = ipb_addr;
+            
+    endtask: ipb_writing
+    
+///////////////////////////////////////    
+    
+    task ipb_read();
+              
+            $display ("%0d ", ipb_rdata); 
+            
+            intf.intf_in_module.ipb_rdata = ipb_addr;            
  
-	task assignInterface( virtual IPbusInterface  IPbusInterface1);
-        
-		this.IPbusInterface1 = IPbusInterface1;    
-    
-	endtask: assignInterface
-	
-	function void write (string filepath, string text); 
-		
-		int fileDescriptor;
-		
-		fileDescriptor = $fopen(filepath, "w");
-		if (fileDescriptor)  $display("File was opened successfully : %0d", fileDescriptor);
-		else     $display("File was NOT opened successfully : %0d", fileDescriptor);
-		
-
-		$fdisplay (fileDescriptor, text);
-
-		$fclose(fileDescriptor);
-		
-	endfunction
-	
-	function string read(string filepath);
-		
-		int fileDescriptor;
-		string line, text;
-		
-		fileDescriptor = $fopen(filepath, "r");
-		if (fileDescriptor)  $display("File was opened successfully : %0d", fileDescriptor);
-		else     $display("File was NOT opened successfully : %0d", fileDescriptor);
-		
-		while (!$feof(fileDescriptor)) begin
-		$fgets(line, fileDescriptor);
-		text = {text, line};
-		$display ("Line: %s", line);
-		end
-		
-		$fclose(fileDescriptor);
-		
-		return text;
-			
-	endfunction
-  
-endclass: IPbusTestClass
-
-
-module TestingIPbusTestClassModule;
-    
-	initial begin
-  
-    IPbusTestClass o1; 
-    
-    string filepath, text;
-    filepath = "C:\Users\Norbert\Vivado\project_7\project_7.srcs\sim_1\new\test.txt";
-    text = "testowanie";
-	
-    o1.write(filepath,text);
-    $display(o1.read(filepath));
-    
-    end
-endmodule: TestingIPbusTestClassModule
-
+    endtask: ipb_read
